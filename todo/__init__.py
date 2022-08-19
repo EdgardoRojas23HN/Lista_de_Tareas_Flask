451695ed2610b3f9148987b91bbaadc1ed959dd4
Edgardo Rojas <erojas504@outlook.com>
2022-08-18 21:39:23 -0600

    Se probo el servidor satisfactoriamente

---
+++ todo/__init__.py
@@ -0,0 +1,25 @@
+import os 
+
+from flask import Flask
+
+def create_app(): #Esta funcion es necesaria para aplicaciones en Flask
+    app = Flask(__name__)
+
+    app.config.from_mapping(
+        SECRET_KEY= 'mikey', #Llave para las sesiones de la app
+        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'), 
+        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'), 
+        DATABASE=os.environ.get('FLASK_DATABASE')              
+    )
+
+    @app.route("/hola") #Ruta de pruebas
+    def hola():
+        return "<p>Hola desde Flask</p>"
+
+
+
+    #if __name__ == "__main__":
+    #    app.run(debug=True)
+
+    return app
