3f6e3877eb7e9c1b8eac9cd60aa231fd538b4d0d
Edgardo Rojas <erojas504@outlook.com>
2022-08-22 11:10:56 -0600

    Se agrego el inicio de sesion

--- todo/auth.py
+++ todo/auth.py
@@ -34,4 +34,36 @@ def register():
                 'insert into user (username, password) values (%s, %s)',
                 (username, generate_password_hash(password))
             )
-            db.commit
+            db.commit()
+
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+        return render_template('auth/register.html')
+
+@bp.route('/login', methods= ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db, c = get_db()
+        error = None
+        c.execute(
+            'select * from user where username = %s', (username,)  
+        )
+        user = c.fetchone()
+
+        if user is None:
+            error = 'Usuario y o contrasena invalida'
+        elif not check_password_hash(user['password'], password):
+            error = 'Usuario y/o contraseña invalida'
+
+            if error is None:
+                session.clear()
+                session['user_id'] = user['id']
+                return redirect(url_for('index'))
+            
+            flash(error)
+
+        return render_template('auth/login.html')
