af789ef4eaedc8059fdf15c5a48c4228a0d228f5
Edgardo Rojas <erojas504@outlook.com>
2022-08-22 20:46:00 -0600

    Se termino el listado

--- todo/todo.py
+++ todo/todo.py
@@ -4,6 +4,17 @@ from flask import (
 
 from werkzeug.exceptions import abort
 from todo.auth import login_required
-from todo.db import get.db
+from todo.db import get_db
 
 bp = Blueprint('todo', __name__)
+
+@bp.route('/')
+@login_required
+def index():
+    db, c = get_db()
+    c.execute(
+        'select t.id, t.description, u.username, t.completed, t.created_at from todo t JOIN user u on t.created_by = u.id order by created_at desc'
+    )
+    todos = c.fetchall()
+
+    return render_template('todo/index.html', todos=todos)
